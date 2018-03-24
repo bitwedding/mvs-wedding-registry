4006415f8ba8c6d69d074d070fe753911bf40fd8
Sven <sven@mutzl.org>
2018-03-24 20:56:12 +0100

    Error handling for create listing

--- api/controllers/ListingCtrl.js
+++ api/controllers/ListingCtrl.js
@@ -5,6 +5,8 @@ var config = require('../config/config.js');
 var Message = require('../models/message.js');
 var Listing = require('../models/listing');
 
+var Helper = require('../libraries/helper.js');
+
 exports.FetchListing = fetch;
 exports.ListListings = list;
 exports.CreateListing = create;
@@ -33,10 +35,16 @@ function create(req, res) {
     var male = req.body.male;
     var female = req.body.female;
     var date = req.body.date;
-    Listing.create(male, female, date)
+    Promise.all([
+            Helper.checkError(male, "ERR_MALE_NAME_MISSING"),
+            Helper.checkError(typeof male == "string" && male.length < 50, "ERR_MALE_NAME_ILLEGAL"),
+            Helper.checkError(female, "ERR_FEMALE_NAME_MISSING"),
+            Helper.checkError(typeof female == "string" && female.length < 50, "ERR_FEMALE_NAME_ILLEGAL"),
+            Helper.checkError(date, "ERR_DATE_MISSING")
+        ])
+        .then(() => Listing.create(male, female, date))
         .then((id) => res.json(Message(1, undefined, id)))
         .catch((error) => {
-            console.log(error);
-            res.status(404).json(Message(0, 'ERR_CREATE_LISTING'));
+            res.status(404).json(Message(0, error.message));
         });
 };
